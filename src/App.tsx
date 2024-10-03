b955daa22d7ad1e81269652de80c01fd8f02131e
Kooribo <maximilianollendorff@gmail.com>
2024-10-03 23:41:16 +0200

    feat: add rough layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Button, Grid2, Stack, Typography } from "@mui/material";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [currentNumber, setCurrentNumber] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleClick = (e: any) => {
+		if (e === "x") {
+			setCurrentNumber("");
+		} else {
+			setCurrentNumber((i) => i + String(e));
+		}
+	};
+
+	return (
+		<>
+			<Stack>
+				<Box
+					sx={{
+						borderRadius: "0rem 0rem 1rem 1rem",
+						bgcolor: "#314735",
+						padding: "0.5rem",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					<Typography
+						textAlign="center"
+						color="white"
+						gutterBottom
+						sx={{ typography: { xs: "h3", md: "h1" } }}
+					>
+						I Know PI
+					</Typography>
+				</Box>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						bgcolor: "#f0f0f0",
+						padding: "1rem",
+						height: "20vh",
+						marginTop: "5rem",
+					}}
+				>
+					<Typography
+						textAlign="center"
+						color="black"
+						gutterBottom
+						sx={{ typography: { xs: "h3", md: "h1" }, flexGrow: 1 }}
+					>
+						π ≈
+					</Typography>
+					<Box
+						sx={{
+							overflowX: "scroll",
+							scrollSnapAlign: "center",
+							display: "flex",
+							flexDirection: "row",
+						}}
+					>
+						{currentNumber.split("").map((item) => (
+							<Typography
+								textAlign="center"
+								color="black"
+								gutterBottom
+								sx={{
+									typography: { xs: "h3", md: "h1" },
+									padding: "0.5rem",
+									tabIndex: "-1",
+								}}
+							>
+								{item}
+							</Typography>
+						))}
+					</Box>
+				</Box>
+				<Box sx={{ marginTop: "2rem" }}>
+					<Grid2 container spacing={2}>
+						{[1, 2, 3, 4, 5, 6, 7, 8, 9, ".", 0, "x"].map((item) => (
+							<Grid2
+								key={item}
+								size={{ xs: 4, md: 1 }}
+								sx={{ display: "flex", justifyContent: "center" }}
+							>
+								<Button
+									sx={{
+										borderRadius: "100%",
+										bgcolor: "#314735",
+										padding: "0.5rem",
+										display: "flex",
+										justifyContent: "center",
+										alignItems: "center",
+									}}
+									onClick={() => handleClick(item)}
+								>
+									<Typography
+										textAlign="center"
+										color="white"
+										gutterBottom
+										sx={{ typography: { xs: "h4", md: "h1" } }}
+									>
+										{item}
+									</Typography>
+								</Button>
+							</Grid2>
+						))}
+					</Grid2>
+				</Box>
+			</Stack>
+		</>
+	);
 }
 
-export default App
+export default App;
